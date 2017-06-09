c3826cb0a9c754daa3ea8f465891f0b2cce5e644
Tianhang Liu <tianhanl@usc.edu>
2017-06-09 21:00:21 +0800

    add a better solution to question 88

--- merge-sorted-array.js
+++ merge-sorted-array.js
@@ -5,6 +5,8 @@
  * @param {number[]} nums2
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
+ *
+ * This solution will run in O(n), but will use O(n) space
  */
 var merge = function (nums1, m, nums2, n) {
   let tempArr = [];
@@ -35,5 +37,25 @@ var merge = function (nums1, m, nums2, n) {
 
   for (let i = 0; i < tempArr.length; i++) {
     nums1[i] = tempArr[i];
+  }
+};
+
+// Another solution using no extra space
+
+const mergeBetter = function (nums1, m, nums2, n) {
+  while (m > 0 && n > 0) {
+    if (nums[m - 1] >= nums2[n - 1]) {
+      nums1[m + n - 1] = nums1[m - 1];
+      m--;
+    } else {
+      nums1[m + n - 1] = nums2[n - 1];
+      n--;
+    }
+  }
+
+  if (n > 0) {
+    for (let i = 0; i < n; i++) {
+      nums1[i] = nums2[i];
+    }
   }
 };
