3ef8b233ec271f8b667e77bc31fe6ad92aedc113
Tianhang Liu <tianhanl@usc.edu>
2017-07-08 07:02:22 +0800

    improve solution to question 63 a little bit by using assigment instead of using addition assigment

--- 63-unique-path-two.js
+++ 63-unique-path-two.js
@@ -11,14 +11,14 @@ var uniquePathsWithObstacles = function (obstacleGrid) {
   obstacleGrid[m - 1][n - 1] = 1;
   for (let i = m - 2; i >= 0; i--) {
     if (obstacleGrid[i][n - 1] !== 1) {
-      obstacleGrid[i][n - 1] += obstacleGrid[i + 1][n - 1]
+      obstacleGrid[i][n - 1] = obstacleGrid[i + 1][n - 1];
     } else {
       obstacleGrid[i][n - 1] = 0;
     }
   }
   for (let i = n - 2; i >= 0; i--) {
     if (obstacleGrid[m - 1][i] !== 1) {
-      obstacleGrid[m - 1][i] += obstacleGrid[m - 1][i + 1];
+      obstacleGrid[m - 1][i] = obstacleGrid[m - 1][i + 1];
     } else {
       obstacleGrid[m - 1][i] = 0;
     }
@@ -26,8 +26,7 @@ var uniquePathsWithObstacles = function (obstacleGrid) {
   for (let i = m - 2; i >= 0; i--) {
     for (let k = n - 2; k >= 0; k--) {
       if (obstacleGrid[i][k] !== 1) {
-        obstacleGrid[i][k] += obstacleGrid[i + 1][k];
-        obstacleGrid[i][k] += obstacleGrid[i][k + 1]
+        obstacleGrid[i][k] += obstacleGrid[i + 1][k] + obstacleGrid[i][k + 1];
       } else {
         obstacleGrid[i][k] = 0;
       }
