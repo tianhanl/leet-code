d076e7be24369cdae6e4ce604fb7e7bd417fffb5
Tianhang Liu <tianhanl@usc.edu>
2017-09-26 11:41:52 -0700

    add improved version of solution to question 350 intersection of two arrays

---
+++ 350-intersection-of-two-arrays-two-improved.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function(nums1, nums2) {
+  // avoid problem cause by default sort function
+  nums1.sort((a, b) => a - b);
+  nums2.sort((a, b) => a - b);
+
+  let i = 0;
+  let j = 0;
+  let result = [];
+
+  while (i < nums1.length && j < nums2.length) {
+    if (nums1[i] === nums2[j]) {
+      result.push(nums1[i]);
+      i++;
+      j++;
+    } else {
+      i += nums1[i] < nums2[j] ? 1 : 0;
+      j += nums2[j] < nums1[i] ? 1 : 0;
+    }
+  }
+  return result;
+};
